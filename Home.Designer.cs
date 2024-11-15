beb10c4905ab49de4c5f2f4920b20391f7fff44d
Adrian <adrian.i.aleksandrov@gmail.com>
2024-11-15 15:18:58 +0100

    Home dashboard layout

---
+++ Home.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace StudentHousing
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn = new Button();
+            panel1 = new Panel();
+            btnLogIn = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btn
+            // 
+            btn.Location = new Point(12, 81);
+            btn.Name = "btn";
+            btn.Size = new Size(451, 336);
+            btn.TabIndex = 0;
+            btn.Text = "button1";
+            btn.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Lime;
+            panel1.Controls.Add(btnLogIn);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1058, 75);
+            panel1.TabIndex = 1;
+            // 
+            // btnLogIn
+            // 
+            btnLogIn.Dock = DockStyle.Right;
+            btnLogIn.Location = new Point(967, 0);
+            btnLogIn.Name = "btnLogIn";
+            btnLogIn.Size = new Size(91, 75);
+            btnLogIn.TabIndex = 1;
+            btnLogIn.Text = "Profile";
+            btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(11, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(482, 54);
+            label1.TabIndex = 0;
+            label1.Text = "Student Housing Platform";
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top;
+            button2.Location = new Point(506, 81);
+            button2.Name = "button2";
+            button2.Size = new Size(465, 336);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 17F);
+            label2.Location = new Point(12, 429);
+            label2.Name = "label2";
+            label2.Size = new Size(362, 46);
+            label2.TabIndex = 7;
+            label2.Text = "Latest Announcements";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(24, 489);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(746, 129);
+            listBox1.TabIndex = 8;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1058, 664);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(panel1);
+            Controls.Add(btn);
+            Name = "Home";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Home";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn;
+        private Panel panel1;
+        private Label label1;
+        private Button btnLogIn;
+        private Button button2;
+        private Label label2;
+        private ListBox listBox1;
+    }
+}
